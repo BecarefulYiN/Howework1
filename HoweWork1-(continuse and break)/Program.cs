e57db39fc5f062eee8f070bfedddeed753f9b708
BecarefulYiN <114420366+BecarefulYiN@users.noreply.github.com>
2024-03-30 20:06:49 +0630

    Add files via upload

---
+++ HoweWork1-(continuse and break)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 0;
+
+        while (i < 5)
+        {
+            Console.WriteLine($"Outer loop iteration {i}");
+
+            i++;
+
+            int j = 0;
+            while (j < 3)
+            {
+                if (j % 2 == 0)
+                {
+                    j++;
+                    continue;
+                }
+                if (i >= 3)
+                {
+                    break; 
+                }
+
+                Console.WriteLine($"Inner loop iteration {j}");
+                j++;
+            }
+        }
+
+        Console.WriteLine("Program has ended.");
+    }
+}
+
